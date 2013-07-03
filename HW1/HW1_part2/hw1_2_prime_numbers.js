d6cf5f02d623d2364c306ce50d4a5cbc4ad2d642
Ubuntu <ubuntu@ip-172-31-42-94.us-west-2.compute.internal>
2013-07-03 02:46:44 +0000

    HW1 both parts

---
+++ HW1/HW1_part2/hw1_2_prime_numbers.js
@@ -0,0 +1,23 @@
+var fs = require('fs');
+fs.writeFileSync( 'PrimeNumbers.txt', listPrimes(100) );
+ 
+
+function listPrimes( nPrimes ) {
+    var primes = [];
+    for( var n = 2;  nPrimes > 0;  n++ ) {
+        if( isPrime( n ) ) {
+            primes.push( n );
+            --nPrimes;
+        }
+    }
+    return primes;
+}
+
+function isPrime( n ) {
+    var max = Math.sqrt(n);
+    for( var i = 2;  i <= max;  i++ ) {
+        if( n % i === 0 )
+            return false;
+    }
+    return true;
+}
